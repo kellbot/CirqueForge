0759420335a924913a95c4b6e048c9cc1647fe41
kellbot <kellbot@gmail.com>
2023-07-10 10:29:15 -0400

    Now pulling live data from google calendar

--- circalendar/src/components/EventSlider.vue
+++ circalendar/src/components/EventSlider.vue
@@ -1,9 +1,10 @@
 <template>
   <v-sheet class="pa-0 mx-auto" max-width="1100px" color="background">
     <v-slide-group multiple show-arrows>
-      <v-slide-group-item v-for="n in upcomingEvents" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
-       
-          <eventCard :logged-in="user.loggedIn" :card-event="n" :is-selected="isSelected" :selected-class="selectedClass" :toggle="toggle"/>
+      <v-slide-group-item v-for="n in realEvents" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
+
+        <eventCard :logged-in="user.loggedIn" :card-event="n" :is-selected="isSelected" :selected-class="selectedClass"
+          :toggle="toggle" />
 
       </v-slide-group-item>
     </v-slide-group>
@@ -23,6 +24,7 @@
   position: absolute;
   width: 100%;
 }
+
 .going {
   z-index: 9999;
   position: absolute;
@@ -40,8 +42,10 @@ import dummyEvents from '@/assets/calendar.json';
 
 import EventCard from '@/components/EventCard.vue';
 
+const appScriptUrl = 'https://script.google.com/macros/s/AKfycbwOSpw-zlGKEQoA8GUmohEdmk1hqszaW6qqG3Pei-E6vOGNnZkBAB-rx66NnS9ywoVcoA/exec?run=ok';
+
 let imgEvents = dummyEvents.map(d => {
-  d.img = d.summary == "Handstand Study Group" ?  hands : aerial;
+  d.img = d.summary == "Handstand Study Group" ? hands : aerial;
   return d;
 });
 
@@ -52,10 +56,20 @@ export default {
       user: user,
       myEvents: [],
       upcomingEvents: imgEvents,
+      realEvents: '',
     }
   },
   components: {
     EventCard,
+  },
+  created() {
+    fetch(appScriptUrl).then(response => response.json())
+    .then(data => {
+      this.realEvents = data.map(d => {
+  d.img = d.summary == "Handstand Study Group" ? hands : aerial;
+  return d;
+});
+    });  
   }
 }
 

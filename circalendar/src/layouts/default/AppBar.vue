b16e200dfb7bedbb5568b409559692cd1e4c0505
kellbot <kellbot@gmail.com>
2023-07-10 17:37:27 -0400

    more return paths

--- circalendar/src/layouts/default/AppBar.vue
+++ circalendar/src/layouts/default/AppBar.vue
@@ -48,7 +48,7 @@ export default {
       logout() {
         auth0.logout({
           logoutParams: {
-            returnTo: window.location.href
+            returnTo: window.location.origin + '/CirqueForge/'
           }
         });
       }

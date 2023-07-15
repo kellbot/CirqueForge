9297ffa37f003281e7bd896fd2c2f3eeaf5ba209
kellbot <kellbot@gmail.com>
2023-07-15 09:12:27 -0400

    fixed commas in yaml

--- circalendar/src/main.js
+++ circalendar/src/main.js
@@ -4,10 +4,7 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 console.log(import.meta.env.MODE)
-console.log(import.meta.env.VITE_VAR)
-console.log(import.meta.env.VITE_ENV)
-console.log(import.meta.env.VITE_SCT)
-console.log(import.meta.env.VITE_TEST)
+
 
 
 // Components
